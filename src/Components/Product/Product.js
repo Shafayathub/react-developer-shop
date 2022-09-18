0c07ba4395e0bbeac53360a537ed47b08b7bc247
Shafayathub <shafayat.ms@gamilcom>
2022-09-18 13:19:13 +0600

    product added

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -1,18 +1,28 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import './Product.css';
 
-const Product = () => {
+const Product = (props) => {
+  const { id, index, img, name } = props.product;
   return (
-    <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
+    <Card style={{ width: '18rem' }} className="product">
+      <Card.Img variant="top" src={img} />
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
+        <Card.Title>{name}</Card.Title>
         <Card.Text>
+          <p>
+            <small>SKU:{index}</small>
+          </p>
+          <p>
+            <small>PI:{id}</small>
+          </p>
           Some quick example text to build on the card title and make up the
           bulk of the card's content.
         </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
       </Card.Body>
+      <Card.Footer>
+        <Button variant="primary">Add To Cart</Button>
+      </Card.Footer>
     </Card>
   );
 };
--- src/Components/Shop/Shop.js
+++ src/Components/Shop/Shop.js
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Product from '../Product/Product';
+import './Shop.css';
 
 const Shop = () => {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    fetch('fakedb.json')
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
   return (
     <Container fluid className="mt-5">
-      <Row>
+      <Row gap={3}>
         <Col xs={9}>
-          <h3>Products</h3>
+          <Container>
+            <Row>
+              {products.map((product) => (
+                <Product key={product.id} product={product}></Product>
+              ))}
+            </Row>
+          </Container>
         </Col>
         <Col>
           <h3>Cart</h3>
