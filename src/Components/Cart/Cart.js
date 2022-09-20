c3b2824fbc86c0f58f5d05bccabaaf4d238262ef
Shafayathub <shafayat.ms@gamilcom>
2022-09-20 14:13:17 +0600

    choose button creating

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,37 +1,61 @@
 import React from 'react';
 import Figure from 'react-bootstrap/Figure';
 import CartData from './CartData';
+import { Button } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBoxOpen, faRefresh } from '@fortawesome/free-solid-svg-icons';
 const Cart = ({ cart }) => {
   const addedProduct = cart;
-  console.log(addedProduct);
   const onlyUnique = (value, index, self) => {
     return self.indexOf(value) === index;
   };
   let unique = addedProduct.filter(onlyUnique);
-
-  console.log(unique);
+  const choose = () => {
+    console.log(unique);
+  };
   if ((unique.length > 0) & (unique.length < 5)) {
     return (
       <div>
         {unique.map((product) => (
           <CartData product={product} key={product.index}></CartData>
         ))}
+        <Button variant="dark text-white mb-2" onClick={choose}>
+          Choose for the Boss{' '}
+          <FontAwesomeIcon icon={faBoxOpen}></FontAwesomeIcon>
+        </Button>
+        <br />
+        <Button variant="dark text-white">
+          Pick Again <FontAwesomeIcon icon={faRefresh}></FontAwesomeIcon>
+        </Button>
       </div>
     );
   } else {
     return (
-      <Figure className="d-flex align-items-center justify-content-center m-3">
-        <Figure.Image
-          width={72}
-          height={72}
-          alt="react.js"
-          src="./../../logo192.png"
-          roundedCircle
-        />
-        <Figure.Caption className="m-1 text-white">
-          Select less than 5 products. <br /> Selected Product: {cart.length}
-        </Figure.Caption>
-      </Figure>
+      <div>
+        {
+          <Figure className="d-flex align-items-center justify-content-center m-3">
+            <Figure.Image
+              width={72}
+              height={72}
+              alt="react.js"
+              src="./../../logo192.png"
+              roundedCircle
+            />
+            <Figure.Caption className="m-1 text-white">
+              Select less than 5 products. <br /> Selected Product:{' '}
+              {cart.length}
+            </Figure.Caption>
+          </Figure>
+        }
+        <Button variant="dark text-white mb-2" onClick={choose}>
+          Choose for the Boss{' '}
+          <FontAwesomeIcon icon={faBoxOpen}></FontAwesomeIcon>
+        </Button>
+        <br />
+        <Button variant="dark text-white">
+          Pick Again <FontAwesomeIcon icon={faRefresh}></FontAwesomeIcon>
+        </Button>
+      </div>
     );
   }
 };
--- src/Components/Shop/Shop.js
+++ src/Components/Shop/Shop.js
@@ -4,13 +4,9 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Product from '../Product/Product';
 import './Shop.css';
-import { Button } from 'react-bootstrap';
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {
-  faBoxOpen,
-  faCartShopping,
-  faRefresh,
-} from '@fortawesome/free-solid-svg-icons';
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
 import Cart from '../Cart/Cart';
 
 const Shop = () => {
@@ -29,6 +25,7 @@ const Shop = () => {
     const newCart = [...cart, product];
     setCart(newCart);
   };
+
   return (
     <Container fluid className="mt-5">
       <Row>
@@ -38,14 +35,6 @@ const Shop = () => {
               Cart <FontAwesomeIcon icon={faCartShopping}></FontAwesomeIcon>
             </h3>
             <Cart cart={cart}></Cart>
-            <Button variant="dark text-white mb-2">
-              Choose for the Boss{' '}
-              <FontAwesomeIcon icon={faBoxOpen}></FontAwesomeIcon>
-            </Button>
-            <br />
-            <Button variant="dark text-white">
-              Pick Again <FontAwesomeIcon icon={faRefresh}></FontAwesomeIcon>
-            </Button>
           </div>
         </Col>
         <Col xs={7} className="mx-2">
